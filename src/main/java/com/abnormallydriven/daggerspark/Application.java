a5ad1e887257341c745b3d631b4c9d577e818ce7
Brett <bretttucker@gmail.com>
2017-02-19 14:32:09 -0700

    Add an in memory people repository

--- src/main/java/com/abnormallydriven/daggerspark/Application.java
+++ src/main/java/com/abnormallydriven/daggerspark/Application.java
@@ -1,7 +1,10 @@
 package com.abnormallydriven.daggerspark;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 
+import com.abnormallydriven.daggerspark.people.PeopleRepository;
 import com.abnormallydriven.daggerspark.people.Person;
 
 import static spark.Spark.*;
@@ -10,10 +13,44 @@ import static spark.Spark.*;
 public class Application {
 
     public static void main(String[] args) {
-        Gson gson = new Gson();
-        get("/hello", (req, res) -> "Hello World");
-        get("/people/:id", "application/json", (req, res) -> {
-            Long personId = Long.valueOf(req.params(":id"));
-            return new Person(personId, "Test", "Person");}, new GsonTransformer(gson));
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        PeopleRepository peopleRepository = new PeopleRepository();
+        GsonTransformer transformer = new GsonTransformer(gson);
+
+
+        post("/people", "application/json", (request, response) -> {
+            JsonObject requestDto = gson.fromJson(request.body(), JsonObject.class);
+            return peopleRepository.createPerson(requestDto.get("firstName").getAsString(), requestDto.get("lastName").getAsString());
+        }, transformer);
+
+
+        get("/people/:id", "application/json", (request, response) -> {
+            Long personId = Long.valueOf(request.params(":id"));
+            return peopleRepository.getPersonById(personId);
+        }, transformer);
+
+        get("/people", "application/json", (request, response) -> {
+            return peopleRepository.getAllPeople();
+        }, transformer);
+
+
+        put("/people/:id", "application/json", (request, response) -> {
+            Long personId = Long.valueOf(request.params(":id"));
+
+            JsonObject requestDto = gson.fromJson(request.body(), JsonObject.class);
+            Person person = peopleRepository.getPersonById(personId);
+
+            person.setFirstName(requestDto.get("firstName").getAsString());
+            person.setLastName(requestDto.get("lastName").getAsString());
+
+            return peopleRepository.updatePerson(person);
+        }, transformer);
+
+
+        delete("/people/:id", "application/json", (request, response) -> {
+            Long personId = Long.valueOf(request.params(":id"));
+            return peopleRepository.deletePerson(personId);
+        }, transformer);
+
     }
 }
---
+++ src/main/java/com/abnormallydriven/daggerspark/people/PeopleRepository.java
@@ -0,0 +1,46 @@
+package com.abnormallydriven.daggerspark.people;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class PeopleRepository {
+
+    private final AtomicInteger idSequence;
+
+    private final Map<Long, Person> personMap;
+
+    public PeopleRepository(){
+        idSequence = new AtomicInteger(0);
+        personMap = new ConcurrentHashMap<>(10);
+    }
+
+    public Person createPerson(String firstName, String lastName){
+        Person newPerson = new Person(idSequence.incrementAndGet(), firstName, lastName);
+        personMap.put(newPerson.getId(), newPerson);
+        return newPerson;
+    }
+
+    public List<Person> getAllPeople(){
+        return personMap.entrySet()
+                .stream()
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+    }
+
+    public Person getPersonById(long personId){
+        return personMap.get(personId);
+    }
+
+    public Person updatePerson(Person updatedPerson){
+        personMap.replace(updatedPerson.getId(), updatedPerson);
+        return updatedPerson;
+    }
+
+    public Person deletePerson(long personId){
+        return personMap.remove(personId);
+    }
+}
