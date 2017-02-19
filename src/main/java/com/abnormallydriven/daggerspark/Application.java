bff5b5284c957c9a17872627b96b2b9efbab5e3f
Brett <bretttucker@gmail.com>
2017-02-19 11:49:06 -0700

    Add the simplest possible endpoint
    We now have a spark application that we can build and execute.

--- src/main/java/com/abnormallydriven/daggerspark/Application.java
+++ src/main/java/com/abnormallydriven/daggerspark/Application.java
@@ -1,8 +1,11 @@
 package com.abnormallydriven.daggerspark;
 
+import static spark.Spark.*;
+
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        get("/hello", (req, res) -> "Hello World");
     }
 }
